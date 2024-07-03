a9141df00cf68e27ffb99bf1a30ffedefcb5b7f2
ilyaSeven7 <ilia.moshtaghi83@gmail.com>
2024-07-03 16:37:36 +0330

    added files

---
+++ src/main/java/me/litedeforged/smputils/SMPUtils.java
@@ -0,0 +1,17 @@
+package me.litedeforged.smputils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
